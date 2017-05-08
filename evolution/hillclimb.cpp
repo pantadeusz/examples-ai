de0deb3d130190394a6c2c8e701c43a7186ff66c
Tadeusz <pantadeusz@pjwstk.edu.pl>
2017-05-08 16:17:44 +0200

    fixed hillclimbing algorithm

--- evolution/hillclimb.cpp
+++ evolution/hillclimb.cpp
@@ -8,6 +8,8 @@
 #include <cmath>
 #include <functional>
 #include <vector>
+#include <list>
+
 
 #include <random>
 #include <chrono>
@@ -24,30 +26,33 @@ double parabola(const vector<double> &solution);
 bool inRange(const vector < double > &v, const vector < double > &minV, const vector < double > &maxV);
 
 /* The randomized hill climbing algorithm */
-vector < double > hillClimbing(const vector< double > solution0, 
+vector < double > hillClimbingRandomized(const vector< double > solution0, 
 							const vector < double > &minV, 
 							const vector < double > & maxV, 
 							function < double (const vector<double>&) > goalF,
 							unsigned generatorSeed = std::chrono::high_resolution_clock::now().time_since_epoch().count());
 
+int main ()  {
+	std::default_random_engine generator(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+//	std::normal_distribution<double> rdist(1.0,2.0); // distribution for neighbour finding
+	std::uniform_real_distribution<double> rdist(-5, 5);
 
-/**********************************************************************/
-
-
-int main (int argc, char **argv)  {
-    srand(time(NULL));
     vector< double > minV = {-5,-5}, maxV = {5,5}; // dziedzina funkcji
-    vector< double > solution = {r(-5, 5), r(-5, 5)}; // punkt startu
+    vector< double > solution = {rdist(generator), rdist(generator)}; // punkt startu
   
-	//solution = hillClimbing(solution, minV, maxV, ackley);
-    solution = hillClimbing(solution, minV, maxV, parabola);
+	//solution = hillClimbingRandomized(solution, minV, maxV, ackley);
+	solution = hillClimbingRandomized(solution, minV, maxV, parabola);
   
-    cout << "And the solution is: " << solution[1] << " , " << solution[1] << endl;
+    cout << "Rozwiązanie: " << solution[1] << " , " << solution[1] << endl;
 
     return 0;
 }
 
-/**********************************************************************/
+
+
+
+
+
 
 /* Ackley test function for optimization */
 double ackley(const vector<double > &solution) {
@@ -57,7 +62,7 @@ double ackley(const vector<double > &solution) {
         exp(0.5*(cos(2.0*M_PI*x)+cos(2*M_PI*y)))
          + M_E + 20);
 }
-/* Simple test function */
+/* Simple test functio */
 double parabola(const vector<double> &solution) {
     return -(solution[0]*solution[0]+solution[1]*solution[1]);
 }
@@ -73,33 +78,33 @@ bool inRange(const vector < double > &v, const vector < double > &minV, const ve
 }
 
 /* The randomized hill climbing algorithm */
-vector < double > hillClimbing(const vector< double > solution0, const vector < double > &minV, const vector < double > & maxV, function < double (const vector<double>&) > goalF, unsigned generatorSeed ) {
+vector < double > hillClimbingRandomized(const vector< double > solution0, const vector < double > &minV, const vector < double > & maxV, function < double (const vector<double>&) > goalF, unsigned generatorSeed ) {
 	std::default_random_engine generator(generatorSeed);
-	//std::normal_distribution<double> distribution(5.0,2.0);
+//	std::normal_distribution<double> rdist(0.0,0.005); // distribution for neighbour finding
 	std::uniform_real_distribution<double> rdist(-0.01, 0.01);
 
-    vector< double > solution = solution0;    
-    int TTL = 20; // no solution improvement after 20 iterations results in algorithm stop
+    list < vector< double > > solution = { solution0 }; // classic approach - store every result in container
+        
+    int TTL = 2000; // no solution improvement after 20 iterations results in algorithm stop
     while (TTL) {
-        auto solutionCandidate = solution;
+        auto solutionCandidate = solution.back();
         for (auto &e: solutionCandidate) {
 			e = e+rdist(generator);  // new solution based on the previous solution
 		}
-        if(!inRange(solutionCandidate, minV, maxV)) {
-            cout << "out of range: " << solution[0] << " , " << solution[1] << endl;
-        } else {
-                auto y = goalF(solution);
-                auto yp = goalF(solutionCandidate);
+        if(inRange(solutionCandidate, minV, maxV)) {
+                auto y = goalF(solution.back()); // current solution quality (previous)
+                auto yp = goalF(solutionCandidate); // new solution quality
                 if (y <= yp) {
-                    solution = solutionCandidate;
+                    solution.push_back(solutionCandidate);
                     TTL = 20;
+					cout << "current solution:" ;
+					for (auto e : solution.back()) cout << " " << e;
+					cout << ": " << y << endl;
                 } else {
+                    solution.push_back(solution.back());
                     TTL--;
                 }
-                cout << "current solution:" ;
-                for (auto e : solution) cout << " " << e;
-                cout << ": " << y << endl;
         }
     }
-	return solution;
+	return solution.back();
 }
