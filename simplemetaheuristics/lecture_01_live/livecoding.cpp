f47b188c466771af8aae45cd8dd31e1d38b19580
Tadeusz <pantadeusz@pjwstk.edu.pl>
2017-09-30 11:13:34 +0200

    algorytm wspinaczkowy

--- simplemetaheuristics/lecture_01_live/livecoding.cpp
+++ simplemetaheuristics/lecture_01_live/livecoding.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <random>
 #include <functional>
+#include <list>
 
 /**
  * 
@@ -23,13 +24,42 @@
  * 
  * */
 
-auto ackleyFunction = [](const vector< double > &p) -> double {
-		double x,y;
-		tie(x,y) = pair<double, double>(p[0], p[1]);
-		return (-20*exp(-0.2*sqrt(0.5*(x*x+y*y)))-
-			exp(0.5*(cos(2.0*M_PI*x)+cos(2*M_PI*y)))
-			 + M_E + 20);
-};
+// auto ackleyFunction = [](const vector< double > &p) -> double {
+// 		double x,y;
+// 		tie(x,y) = pair<double, double>(p[0], p[1]);
+// 		return (-20*exp(-0.2*sqrt(0.5*(x*x+y*y)))-
+// 			exp(0.5*(cos(2.0*M_PI*x)+cos(2*M_PI*y)))
+// 			 + M_E + 20);
+// };
+
+
+
+using namespace std;
+
+
+
+
+template < class Solution >
+Solution generalProblemSolver(Solution s0,
+		function < double (Solution) > fitness,
+		function < bool (Solution) > termCondition,
+		function < Solution (Solution) > getNeighbour
+	) {
+	int i = 0;
+	std::list < Solution > s;
+	s.push_back(s0);
+	// warunek zakończenia
+	while (!termCondition(s.back())) {
+		auto newSolution = getNeighbour(s.back());
+		if (fitness(newSolution) >= fitness(s.back()) ) {
+			s.push_back(newSolution);
+		} else {
+			s.push_back(s.back());
+		}
+	}
+	return s.back();
+}
+
 
 
 /**
@@ -39,6 +69,23 @@ auto ackleyFunction = [](const vector< double > &p) -> double {
  * */
 
 int main(int argc, char **argv) {
+	// funkcja optymalizowana
+	auto fit = [](double x)->double{
+		return 100+-100*x*x;
+	};
+	double x0 = ((double)rand()/RAND_MAX)*4-2;
+	int timeToGo = 100000;
+	auto termCond = [&](double s)->bool{
+		timeToGo--;
+		return timeToGo <= 0;
+	};
+	auto getNeighbour = [](double s) {
+		return s + (double)(rand()%1000)/100000.0 - 0.005;
+	};
+
+	cout << "x0 = " << x0 << endl;
+	double foundX = generalProblemSolver<double>( x0,fit,termCond,getNeighbour);
+	cout << "solution = " << foundX << endl;
 	return 0;
 }
 
