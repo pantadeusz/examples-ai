1b6bf3a5023bfa5ecdc004bba649a16e0c229724
pantadeusz <pantadeusz+git@pjwstk.edu.pl>
2021-01-20 20:25:52 +0100

    astarbmp - version with set

--- pathfinding/astarbmp/astarbmp.cpp
+++ pathfinding/astarbmp/astarbmp.cpp
@@ -15,17 +15,17 @@
 #include <list>
 #include <map>
 #include <numeric>
-#include <set>
 #include <queue>
+#include <set>
 #include <vector>
 
 using std::function;
 using std::list;
 using std::map;
 using std::max_element;
+using std::priority_queue;
 using std::remove_if;
 using std::reverse;
-using std::priority_queue;
 using std::set;
 using std::string;
 using std::vector;
@@ -40,17 +40,18 @@ using std::vector;
  * @return Container
  */
 template <class Node, class Container>
-Container reconstructPath(map<Node, Node> &came_from, Node goal) {
-  Container reconstructed_path;
-  Node current = goal;
-  reconstructed_path.push_back(current);
-  while (came_from.count(current) > 0) {
-    current = came_from[current];
+Container reconstructPath(map<Node, Node>& came_from, Node goal)
+{
+    Container reconstructed_path;
+    Node current = goal;
     reconstructed_path.push_back(current);
-  }
-  reverse(reconstructed_path.begin(),
-          reconstructed_path.end()); // 9 8 7 6 5 4 3 2 1
-  return reconstructed_path;
+    while (came_from.count(current) > 0) {
+        current = came_from[current];
+        reconstructed_path.push_back(current);
+    }
+    reverse(reconstructed_path.begin(),
+        reconstructed_path.end()); // 9 8 7 6 5 4 3 2 1
+    return reconstructed_path;
 }
 
 /**
@@ -65,54 +66,69 @@ Container reconstructPath(map<Node, Node> &came_from, Node goal) {
  * @param accessible_verts the function that returns accesible nodes from the given node
  * @return Path the generated path
  */
-template <class Node, class Path = std::list<Node> >
+template <class Node, class Path = std::list<Node>>
 Path searchPath(
-    const Node &start, ///< start point
-    const Node &goal,  ///< goal point
-    const function<float(const Node &, const Node &)>
-        dist, ///< actual distance between adjacent points
-    const function<float(const Node &, const Node &)> h, ///< heuristic function
-    const function<list<Node>(const Node &)>
+    const Node& start, ///< start point
+    const Node& goal,  ///< goal point
+    const function<float(const Node&, const Node&)>
+        dist,                                          ///< actual distance between adjacent points
+    const function<float(const Node&, const Node&)> h, ///< heuristic function
+    const function<list<Node>(const Node&)>
         accessible_verts ///< returns accessible vertices
-) {
-  set<Node> closedSet;
-  map<Node, Node> came_from;
-  map<Node, float> g_score;
-  map<Node, float> f_score;
-  auto cmp = [&f_score](Node best, Node b) { return f_score[best] < f_score[b]; };
-  std::priority_queue<Node, std::vector<Node>, decltype(cmp)> openSet(cmp);
+)
+{
+    set<Node> closedSet;
+    map<Node, Node> came_from;
+    map<Node, float> g_score;
+    map<Node, float> f_score;
+    //auto cmp = [&f_score](Node best, Node b) { return f_score[best] > f_score[b]; };
+    //std::priority_queue<Node, std::vector<Node>, decltype(cmp)> openSet(cmp);
+    //openSet.push(start);
+    set<Node> openSet;
+    openSet.insert(start);
+    g_score[start] = 0;                  ///< distance from start
+    f_score[start] = 0 + h(start, goal); ///< estimate distancd to goal
 
-  openSet.push(start);
-  g_score[start] = 0;                  ///< distance from start
-  f_score[start] = 0 + h(start, goal); ///< estimate distancd to goal
+    int t = 0;
+    while (openSet.size() > 0) {
+        /// searching openSet element with lowest f_score and saving it to "best"
+// priority queue //        const Node best = openSet.top();
+// priority queue //        openSet.pop(); ///< we took the best, so it is no longer in open set
 
-  while (openSet.size() > 0) {
-    /// searching openSet element with lowest f_score and saving it to "best"
-    const Node best = openSet.top();
-    openSet.pop(); ///< we took the best, so it is no longer in open set
-    if (best == goal)
-      return reconstructPath<Node, Path>(came_from, goal);
-    closedSet.insert(best);
-    /// check every possible direction
-    for (const Node &neighbor : accessible_verts(best)) {
-      /// not in closed set?
-      if (closedSet.count(neighbor) == 0) {
-        /// calculate temporary t_g_score that is the sum of g_score of current
-        /// node (best) and actual distance between (best-neighbour)
-        float t_g_score = g_score[best] + dist(neighbor, best);
-        /// we should put neighbor to current openSet - it can be evaluated
-        /// later
-        openSet.push(neighbor);
-        /// if the neighbor does not exist, we assume that it is with inf value
-        if ((g_score.count(neighbor) == 0) || (t_g_score < g_score[neighbor])) {
-          came_from[neighbor] = best;
-          g_score[neighbor] = t_g_score;
-          f_score[neighbor] = g_score[neighbor] + h(neighbor, goal);
+
+        const Node best = *std::max_element(openSet.begin(), openSet.end(), [&f_score](Node best, Node b) { return f_score[best] > f_score[b]; });
+        openSet.erase(best); ///< we took the best, so it is no longer in open set
+
+        if (best == goal)
+            return reconstructPath<Node, Path>(came_from, goal);
+        closedSet.insert(best);
+        /// check every possible direction
+        for (const Node& neighbor : accessible_verts(best)) {
+            /// not in closed set?
+            if (closedSet.count(neighbor) == 0) {
+                /// calculate temporary t_g_score that is the sum of g_score of current
+                /// node (best) and actual distance between (best-neighbour)
+                float t_g_score = g_score[best] + dist(neighbor, best);
+                /// we should put neighbor to current openSet - it can be evaluated
+                /// later
+                openSet.insert(neighbor);
+// priority queue //                openSet.push(neighbor);
+                
+                /// if the neighbor does not exist, we assume that it is with inf value
+                if ((g_score.count(neighbor) == 0) || (t_g_score < g_score[neighbor])) {
+                    came_from[neighbor] = best;
+                    g_score[neighbor] = t_g_score;
+                    f_score[neighbor] = g_score[neighbor] + h(neighbor, goal);
+                }
+            }
         }
-      }
-    } }
-  return {}; 
- /// no path found
+        t++;
+        if ((t%10000) == 0) {
+          std::cout << "openSet.size() = " << openSet.size()  << std::endl;
+        }
+    } 
+    return {};
+    /// no path found
 }
 
 ////////////////////////// END OF A*  /////////////////////
@@ -121,28 +137,34 @@ Path searchPath(
  * class representing node in graph. In this particular case it is point_t.
  * */
 
-class point_t : public std::array<int, 2> {
+class point_t : public std::array<int, 2>
+{
 public:
-  point_t(int x = 0, int y = 0) : std::array<int, 2>() {
-    (*this)[0] = x;
-    (*this)[1] = y;
-  };
+    point_t(int x = 0, int y = 0) : std::array<int, 2>()
+    {
+        (*this)[0] = x;
+        (*this)[1] = y;
+    };
 };
 namespace std {
 /**
  * @brief hash function for point_t
  */
-template <> struct hash<point_t> {
-  size_t operator()(const point_t &k) const {
-    return ((k[0] << sizeof(k[0]) * 4) | (k[0] >> sizeof(k[0]) * 4)) ^ k[1];
-  }
+template <>
+struct hash<point_t> {
+    size_t operator()(const point_t& k) const
+    {
+        return ((k[0] << sizeof(k[0]) * 4) | (k[0] >> sizeof(k[0]) * 4)) ^ k[1];
+    }
 };
 } // namespace std
 
 /**
  * class representing path
  * */
-class path_t : public list<point_t> {};
+class path_t : public list<point_t>
+{
+};
 
 /**
  * we could use some different tile type. Here is just a char that represents
@@ -153,75 +175,80 @@ typedef unsigned char tile_t;
  * helper class for image support. This just wraps lodepng library in more OOP
  * way.
  * */
-class image_t : public vector<tile_t> {
+class image_t : public vector<tile_t>
+{
 public:
-  unsigned int width;
-  unsigned int height;
-  tile_t &operator()(const point_t &p) { return (*this)[p[1] * width + p[0]]; }
-  tile_t operator()(const point_t &p) const {
-    return (*this)[p[1] * width + p[0]];
-  }
+    unsigned int width;
+    unsigned int height;
+    tile_t& operator()(const point_t& p) { return (*this)[p[1] * width + p[0]]; }
+    tile_t operator()(const point_t& p) const
+    {
+        return (*this)[p[1] * width + p[0]];
+    }
 
-  void save(const string &fname) {
-    image_t image = *this;
-    unsigned error =
-        lodepng::encode(fname, (unsigned char *)image.data(), image.width,
-                        image.height, LodePNGColorType::LCT_GREY, 8);
-    if (error)
-      throw std::runtime_error(lodepng_error_text(error));
-  }
+    void save(const string& fname)
+    {
+        image_t image = *this;
+        unsigned error =
+            lodepng::encode(fname, (unsigned char*)image.data(), image.width,
+                image.height, LodePNGColorType::LCT_GREY, 8);
+        if (error)
+            throw std::runtime_error(lodepng_error_text(error));
+    }
 
-  static image_t load(const string &fname) {
-    image_t image;
-    unsigned error = lodepng::decode(image, image.width, image.height, fname,
-                                     LodePNGColorType::LCT_GREY, 8);
-    if (error)
-      throw std::runtime_error(lodepng_error_text(error));
-    return image;
-  }
+    static image_t load(const string& fname)
+    {
+        image_t image;
+        unsigned error = lodepng::decode(image, image.width, image.height, fname,
+            LodePNGColorType::LCT_GREY, 8);
+        if (error)
+            throw std::runtime_error(lodepng_error_text(error));
+        return image;
+    }
 };
 
-int main(int argc, char **argv) {
-  auto img = image_t::load("sample.png");
+int main(int argc, char** argv)
+{
+    auto img = image_t::load("sample.png");
 
-  point_t start(21, 17);
-  point_t goal(211, 132);
+    point_t start(21, 17);
+    point_t goal(211, 132);
 
-  /**
+    /**
    * function that returns the list of accessible vertices from the current.
    * It takes current coordinates and returns coordinate shifted by the
    * direction. It also checks if we are in the image or outside.
    * */
-  const function<list<point_t>(const point_t &)> accessible_verts =
-      [&](const point_t &best) -> list<point_t> {
-    list<point_t> ret = {{+0, -1}, {-1, -1}, {-1, +0}, {-1, +1},
-                         {+0, +1}, {+1, +1}, {+1, +0}, {+1, -1}};
-    for (auto &e : ret)
-      e = {best[0] + e[0], best[1] + e[1]};
-    ret.erase(remove_if(ret.begin(), ret.end(),
-                        [&img](const point_t &candidate_node) {
+    const function<list<point_t>(const point_t&)> accessible_verts =
+        [&](const point_t& best) -> list<point_t> {
+        list<point_t> ret = {{+0, -1}, {-1, -1}, {-1, +0}, {-1, +1},
+            {+0, +1}, {+1, +1}, {+1, +0}, {+1, -1}};
+        for (auto& e : ret)
+            e = {best[0] + e[0], best[1] + e[1]};
+        ret.erase(remove_if(ret.begin(), ret.end(),
+                      [&img](const point_t& candidate_node) {
                           // remove everything that is incorrect
                           return (candidate_node[0] < 0) ||
                                  (candidate_node[1] < 0) ||
                                  (candidate_node[0] >= img.width) ||
                                  (candidate_node[1] >= img.height) ||
                                  (img(candidate_node) >= 128);
-                        }),
-              ret.end());
-    return ret;
-  };
-  auto heuristic_f = [&](const point_t &a, const point_t &b) -> float {
-    return ::sqrt((a[0] - b[0]) * (a[0] - b[0]) +
-                  (a[1] - b[1]) * (a[1] - b[1]));
-  };
-  auto dist_f = heuristic_f;
+                      }),
+            ret.end());
+        return ret;
+    };
+    auto heuristic_f = [&](const point_t& a, const point_t& b) -> float {
+        return ::sqrt((a[0] - b[0]) * (a[0] - b[0]) +
+                      (a[1] - b[1]) * (a[1] - b[1]));
+    };
+    auto dist_f = heuristic_f;
 
-  for (auto p : searchPath<point_t>(start, goal, dist_f, heuristic_f,
-                                            accessible_verts)) {
-    static int color = 0;
-    color = (color + 16) % 128;
-    img(p) = 128 + color; //; ///< draw path
-  }
-  img.save("result.png");
-  return 0;
+    for (auto p : searchPath<point_t>(start, goal, dist_f, heuristic_f,
+             accessible_verts)) {
+        static int color = 0;
+        color = (color + 16) % 128;
+        img(p) = 128 + color; //; ///< draw path
+    }
+    img.save("result.png");
+    return 0;
 }
