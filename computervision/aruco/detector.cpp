7b063692bad7615fae504f1ddb52dab601ddd263
pantadeusz <pantadeusz+git@pjwstk.edu.pl>
2022-12-14 14:58:58 +0100

    po wykladzie

--- computervision/aruco/detector.cpp
+++ computervision/aruco/detector.cpp
@@ -2,24 +2,69 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/opencv.hpp>
 
+#include <chrono>
+#include <list>
+
 
 // g++ `pkg-config --cflags opencv4` detector.cpp -o detector `pkg-config --libs opencv4`
 // https://docs.opencv.org/4.x/d5/dae/tutorial_aruco_detection.html
+const double MARKER_MM = 30;
+
+std::vector<cv::Point2f> fromPixelsToMm(std::vector<cv::Point2f> original)
+{
+    std::vector<cv::Point2f> result;
+    double distances = 0;
+    for (int i = 0; i < 4; i++) {
+        cv::Point2f v = (original[i] - original[(i + 1) % 4]);
+
+        distances += std::sqrt(v.x * v.x + v.y * v.y);
+    }
+    distances = distances / 4;
+
+    for (int i = 0; i < 4; i++) {
+        result.push_back({MARKER_MM * (original[i].x / distances), MARKER_MM * (original[i].y / distances)});
+    }
+    return result;
+}
 
 int main()
 {
-    cv::VideoCapture cam(0);
-    while (cv::waitKey(10) != 27) {
-        cv::Mat inputImage;
+    using namespace cv;
+    using namespace std;
+
+    list<pair<std::chrono::steady_clock::time_point, vector<Point2f>>> detectedPositions;
+
+    VideoCapture cam(0);
+    Ptr<aruco::DetectorParameters> parameters = aruco::DetectorParameters::create();
+    Ptr<aruco::Dictionary> dictionary = aruco::getPredefinedDictionary(aruco::DICT_6X6_250);
+    namedWindow("markers", WINDOW_NORMAL);
+    namedWindow("markers_rejected", WINDOW_NORMAL);
+    while (waitKey(10) != 27) {
+        Mat inputImage;
+        Mat detected;
         cam >> inputImage;
-        std::vector<int> markerIds;
-        std::vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
-        cv::Ptr<cv::aruco::DetectorParameters> parameters = cv::aruco::DetectorParameters::create();
-        cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
-        cv::aruco::detectMarkers(inputImage, dictionary, markerCorners, markerIds, parameters, rejectedCandidates);
-        cv::Mat outputImage = inputImage.clone();
-        cv::aruco::drawDetectedMarkers(outputImage, markerCorners, markerIds);
-        cv::imshow("markers",outputImage);
+        detected = inputImage.clone();
+        vector<int> markerIds;
+        vector<vector<Point2f>> markerCorners, rejectedCandidates;
+        aruco::detectMarkers(inputImage, dictionary, markerCorners,
+            markerIds, parameters, rejectedCandidates);
+        Mat outputImage = inputImage.clone();
+        aruco::drawDetectedMarkers(detected, markerCorners, markerIds);
+        auto found = find(markerIds.begin(), markerIds.end(), 0);
+        if (found != markerIds.end()) {
+            int foundIdx = distance(markerIds.begin(), found);
+            cout << foundIdx << endl;
+            auto found = markerCorners.at(foundIdx);
+            std::cout << found << std::endl;
+            // drawContours(detected, markerCorners, -1, {0, 0, 255}, 2);
+            auto p = found[0];
+            auto r = fromPixelsToMm(found)[0];
+            string txt = "[" + to_string(p.x) + "," + to_string(p.y) + "] [" + to_string(r.x) + "," + to_string(r.y) + "]";
+               putText(detected, txt.c_str(), p,
+            FONT_HERSHEY_PLAIN, 0.5, {0, 255, 0});
+        }
+        imshow("markers", detected);
     }
+
     return 0;
 }
--- computervision/aruco/markers.cpp
+++ computervision/aruco/markers.cpp
@@ -4,23 +4,24 @@
 // g++ `pkg-config --cflags opencv4` markers.cpp -o markers `pkg-config --libs opencv4`
 // https://docs.opencv.org/4.x/d5/dae/tutorial_aruco_detection.html
 
-const int COLS = 8;
-const int ROWS = 10;
+const int COLS = 4;
+const int ROWS = 6;
 const int SIDE = 100;
 const int BORDER = 50;
 
 int main()
 {
-    cv::Mat1f ret_image(ROWS * (SIDE + BORDER), COLS * (SIDE + BORDER), 255.0);
+    using namespace cv;
+    Ptr<aruco::Dictionary> dictionary = aruco::getPredefinedDictionary(aruco::DICT_7X7_250);
+    Mat1f ret_image(ROWS * (SIDE + BORDER), COLS * (SIDE + BORDER), 255.0);
     for (int x = 0; x < COLS; x++) {
         for (int y = 0; y < ROWS; y++) {
             int i = y * COLS + x;
-            cv::Mat markerImage;
-            cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
-            cv::aruco::drawMarker(dictionary, i, SIDE, markerImage, 1);
-            cv::Rect roi(cv::Point(BORDER / 2 + x * (SIDE + BORDER), BORDER / 2 + y * (SIDE + BORDER)), markerImage.size());
+            Mat markerImage;
+            aruco::drawMarker(dictionary, i, SIDE, markerImage, 1);
+            Rect roi(Point(BORDER / 2 + x * (SIDE + BORDER), BORDER / 2 + y * (SIDE + BORDER)), markerImage.size());
             markerImage.copyTo(ret_image(roi));
         }
     }
-    cv::imwrite("markers.png", ret_image);
+    imwrite("markers_7_7.png", ret_image);
 }
