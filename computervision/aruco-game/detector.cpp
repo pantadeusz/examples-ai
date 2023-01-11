8c15e1addc6d314228b7fb913f913c8c13629e93
pantadeusz <pantadeusz+git@pjwstk.edu.pl>
2023-01-11 14:32:35 +0100

    example

--- computervision/aruco-game/detector.cpp
+++ computervision/aruco-game/detector.cpp
@@ -1,17 +1,18 @@
+#include <chrono>
+#include <list>
 #include <opencv2/aruco.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/opencv.hpp>
+#include <random>
 #include <tuple>
-#include <chrono>
-#include <list>
+
+std::random_device rd;
+std::mt19937 generator(rd());
 
 
 // wyklad:
 // Poza: https://docs.opencv.org/4.x/d5/d1f/calib3d_solvePnP.html
-//  
-
-
-
+// O detektorze https://docs.opencv.org/3.4/d5/dae/tutorial_aruco_detection.html
 
 
 // g++ `pkg-config --cflags opencv4` detector.cpp -o detector `pkg-config --libs opencv4`
@@ -57,12 +58,29 @@ int main()
     auto [camera_matrix, dist_coeffs] = get_camera_properties_ps3eye();
     auto obj_points = get_object_points();
 
+    Mat car_image = imread("car0.png");
+    Mat goal_image = imread("obstacle.png");
+    Vec2f car_position(200, 200);
+
+    vector<Vec2f> goals;
+    int counter = 0;
+    int points = 0;
     while (waitKey(10) != 27) {
         Mat inputImage;
         Mat detected;
         cam >> inputImage;
         //resize(inputImage, inputImage, Size(320, 240), 0, 0, INTER_CUBIC);
         detected = inputImage.clone();
+
+        if ((counter++ % 20 == 0) && (goals.size() < 10)) {
+            std::uniform_int_distribution<int> distr_x(0 + goal_image.cols, detected.cols - goal_image.cols);
+            std::uniform_int_distribution<int> distr_y(0 + goal_image.rows, detected.rows - goal_image.rows);
+            Vec2f p;
+            p.val[0] = distr_x(generator);
+            p.val[1] = distr_y(generator);
+            goals.push_back(p);
+        }
+
         vector<int> markerIds;
         vector<vector<Point2f>> markerCorners, rejectedCandidates;
         aruco::detectMarkers(inputImage, dictionary, markerCorners,
@@ -74,24 +92,51 @@ int main()
         if (found != markerIds.end()) {
             int foundIdx = distance(markerIds.begin(), found);
             auto found = markerCorners.at(foundIdx);
-            
+
             cv::Vec3d rvecs, tvecs;
             cv::solvePnP(obj_points, found, camera_matrix, dist_coeffs, rvecs, tvecs);
             cv::drawFrameAxes(detected, camera_matrix, dist_coeffs, rvecs, tvecs, 10.1);
 
-            if (false) {
             Mat rot_mat;
             Rodrigues(rvecs, rot_mat);
-            double angle_z = atan2(rot_mat.at<double>(1,0), rot_mat.at<double>(0,0));
-            double angle_x = atan2(rot_mat.at<double>(2,1), rot_mat.at<double>(2,2));
-            angle_x = -angle_x+((angle_x < 0)?(-M_PI):M_PI);
-            double angle_y = -asin(rot_mat.at<double>(2,0));
+            double angle_z = atan2(rot_mat.at<double>(1, 0), rot_mat.at<double>(0, 0));
+            double angle_x = atan2(rot_mat.at<double>(2, 1), rot_mat.at<double>(2, 2));
+            angle_x = -angle_x + ((angle_x < 0) ? (-M_PI) : M_PI);
+            double angle_y = -asin(rot_mat.at<double>(2, 0));
 
-            std::cout << angle_x << " " << angle_y << " " << angle_z << std::endl;
+            //std::cout << angle_x << " " << angle_y << " " << angle_z << std::endl;
+            auto new_pos = car_position;
+            new_pos.val[0] += 10.0 * angle_y;
+            new_pos.val[1] -= 10.0 * angle_x;
+            auto new_pos_scr = new_pos;
+            new_pos_scr.val[0] -= car_image.cols / 2;
+            new_pos_scr.val[1] -= car_image.rows / 2;
+            if ((new_pos_scr.val[0] > 0) && (new_pos_scr.val[0] < (detected.cols - car_image.cols - 1)) && (new_pos_scr.val[1] > 0) && (new_pos_scr.val[1] < (detected.rows - car_image.rows - 1))) {
+                car_position = new_pos;
             }
         }
-        //Mat insetImage(detected, Rect(car_pos-(car_img.cols/2), 160, car_img.rows, car_img.cols));
-        //car_img.copyTo(insetImage);
+        Mat insetImage(detected,
+            Rect(
+                car_position.val[0] - (car_image.cols / 2),
+                car_position.val[1] - (car_image.rows / 2),
+                car_image.rows, car_image.cols));
+        car_image.copyTo(insetImage);
+        for (auto goal : goals) {
+            Mat insetImage(detected,
+                Rect(
+                    goal.val[0] - (goal_image.cols / 2),
+                    goal.val[1] - (goal_image.rows / 2),
+                    goal_image.rows, goal_image.cols));
+            goal_image.copyTo(insetImage);
+        }
+        for (int i = 0; i < goals.size(); i++) {
+            if (cv::norm(goals[i] - car_position) < (goal_image.cols / 2)) {
+                points++;
+                std::cout << points << std::endl;
+                goals.erase(goals.begin() + i);
+                i--;
+            }
+        }
         imshow("markers", detected);
     }
 
