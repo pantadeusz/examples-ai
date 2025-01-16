d700c35791a3035e31ddb4ec2c64906a032e0a86
pantadeusz <pantadeusz+git@pjwstk.edu.pl>
2025-01-16 09:47:14 +0100

    ollama demo

--- dnn/pytorch2/hello3autoenc.py
+++ dnn/pytorch2/hello3autoenc.py
@@ -7,6 +7,8 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 from torchvision.utils import save_image
+from torchvision.transforms import v2
+from torchvision.transforms import InterpolationMode
 
 # Download training data from open datasets.
 training_data = datasets.FashionMNIST(
@@ -24,10 +26,15 @@ test_data = datasets.FashionMNIST(
     transform=ToTensor(),
 )
 
-batch_size = 1000
+batch_size = 5000
 
-raw_training_data = [ (elem[0],elem[0]) for elem in training_data ]
-raw_test_data = [ (elem[0],elem[0]) for elem in test_data ]
+#raw_training_data = [ (elem[0],elem[0]) for elem in training_data ]
+#raw_training_data = [ (training_data[i >> 1][0] * ((i%2)*2-1.0),training_data[i >> 1][0]) for i in range(len(training_data)*2) ]
+raw_training_data = [ (training_data[i % len(training_data)][0],training_data[i % len(training_data)][0]) for i in range(len(training_data)*5) ]
+rotater = v2.RandomRotation(degrees=(0, 359), interpolation=InterpolationMode.BILINEAR)
+hflipper = v2.RandomHorizontalFlip(p=0.5)
+raw_training_data = [(hflipper(rotater(a)),b) for a,b in raw_training_data]
+raw_test_data = [ (rotater(elem[0]),elem[0]) for elem in test_data ]
 # Create data loaders.
 train_dataloader = DataLoader(raw_training_data, batch_size=batch_size, shuffle=True)
 test_dataloader = DataLoader(raw_test_data, batch_size=batch_size, shuffle=True )
@@ -35,6 +42,7 @@ test_dataloader = DataLoader(raw_test_data, batch_size=batch_size, shuffle=True 
 for X, Y in test_dataloader:
     print(f"Shape of X [N, C, H, W]: {X.shape} {X.dtype}")
     print(f"Shape of y: {Y.shape} {Y.dtype}")
+    save_image(X[1],f'sample_mnist.png')
     break
 
 # Get cpu, gpu or mps device for training.
@@ -53,33 +61,31 @@ class NeuralNetwork(nn.Module):
         super().__init__()
         
         self.cnn_stack = nn.Sequential(
-            nn.Conv2d(1,32,(3,3)), # 26*26*32
+            nn.Conv2d(1,32,(5,5)), # 24*24*32
             nn.ReLU(),
-            nn.MaxPool2d(2), # 13 * 13* 32
-            nn.Conv2d(32,32,(3,3)), # 11 * 11 * 32
+            nn.MaxPool2d(2), # 12 * 12* 32
+            nn.Conv2d(32,32,(3,3)), # 10 * 10 * 32
             nn.ReLU(),
+            nn.Dropout(0.2),
         )
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(11*11*32, 512),
-            #nn.Linear(28*28, 512),# This was in original example
+            nn.Linear(10*10*32, 1024),
             nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(512, 32),
+            nn.Linear(1024, 28*28),
             nn.Sigmoid()
         )
         self.decoder_stack = nn.Sequential(
-            nn.Linear(32, 1024),
+            nn.Linear(28*28, 1024),
             nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(1024, 800),
+            nn.Dropout(0.2),
+            nn.Linear(1024, 2048),
             nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(800, 28*28),
-            nn.Unflatten(1,(1,28,28))
+            nn.Dropout(0.2),
+            nn.Linear(2048, 26*26*32),
+            nn.ReLU(),
+            nn.Unflatten(1,(32,26,26)),
+            nn.ConvTranspose2d(32,1,(3,3)) # conv_transpose2d(inputs, weights, padding=1)
         )
 
     def forward(self, x):
@@ -112,7 +118,7 @@ def train(dataloader, model, loss_fn, optimizer):
         optimizer.step()
         optimizer.zero_grad()
 
-        if batch % 100 == 0:
+        if batch % 10 == 0:
             loss, current = loss.item(), (batch + 1) * len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
@@ -130,7 +136,7 @@ def test(dataloader, model, loss_fn):
     print(f"Test Error: Avg loss: {test_loss:>8f} of {size} items\n")
 
 
-epochs = 1000
+epochs = 200
 for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
     train(train_dataloader, model, loss_fn, optimizer)
@@ -147,7 +153,7 @@ eval_model = model.eval() # end of training
 
 with torch.no_grad():
     i = 0
-    for test_row in test_data:
+    for test_row in raw_test_data:
         i = i + 1
         x, y = test_row[0], test_row[1]
         x = torch.reshape(x, (1,1,28,28))
@@ -155,6 +161,7 @@ with torch.no_grad():
         pred = eval_model(x)
         save_image(pred,f'results/mnist_{i}_y.png')
         save_image(x,f'results/mnist_{i}_x.png')
+        if i % 1000 == 0: print(str(i) + " / " + str(len(test_data)))
 
 
 
---
+++ llm-ollama-huggingfaces/ollama-local-demo-streamlit/app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import requests
+import json
+
+
+st.title("LLM Chat")
+query = st.text_area("Enter your message:",key='prompt_text_area')
+if st.button("Send") and query:
+	with st.empty():  # Placeholder for streaming response
+		try:
+			if 'context' not in st.session_state:
+				response = requests.post(
+					"http://localhost:11434/api/generate",
+					json={
+						"model": "wizardlm2:latest",  # Replace with the specific model you want to use
+						"prompt": query,
+						"temperature": 0.7,  # Adjust as needed
+						"max_tokens": 200,  # Set token limit
+						"stream": True
+					},
+					stream=True
+				)
+			else:
+				response = requests.post(
+					"http://localhost:11434/api/generate",
+					json={
+						"model": "wizardlm2:latest",  # Replace with the specific model you want to use
+						"prompt": query,
+						"temperature": 0.7,  # Adjust as needed
+						"max_tokens": 200,  # Set token limit
+						"stream": True,
+						"context": st.session_state["context"]  # Include context
+					},
+					stream=True
+				)			
+			if response.status_code == 200:
+				i = 0
+				result = ""
+				for line in response.iter_lines(decode_unicode=True):
+					if line.strip():
+						try:
+							data = json.loads(line)
+							if "response" in data:
+								result += data["response"]
+								i = i + 1
+								st.text_area("Response:", result, height=200, key='dynamic_response_'+str(i))
+							if "context" in data:
+								if 'context' not in st.session_state:
+									st.session_state['context'] = data["context"]
+								
+						except json.JSONDecodeError:
+							# Handle any malformed lines gracefully
+							st.text_area("Response:", f"Error decoding JSON: {line}", height=200, key='response_error')
+			else:
+				st.text_area("Response:", f"Error: {response.status_code} - {response.text}", height=200)
+		except Exception as e:
+			st.text_area("Response:", f"An error occurred: {e}", height=200, key='response_error')
+			
